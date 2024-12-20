3e46c33b6de595d19904c0f6acb627bb63549ce4
Shri <sridheepan@gmail.com>
2024-12-19 19:21:36 -0500

    added hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 'use client';
-import { ConfigProvider, Menu } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import { ConfigProvider, Drawer, Menu } from 'antd';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
@@ -46,6 +47,7 @@ const Header = () => {
   ];
 
   const [current, setCurrent] = useState('mail');
+  const [drawerVisible, setDrawerVisible] = useState(false); // State to control drawer visibility
 
   const onClick = (e) => {
     console.log('click ', e);
@@ -54,11 +56,23 @@ const Header = () => {
 
   const router = useRouter();
 
+  const handleDrawerToggle = () => {
+    setDrawerVisible(!drawerVisible); // Toggle the drawer visibility
+  };
+
   return (
     <div className='w-screen flex bg-menu items-center h-[70px] md:px-[100px] sm:px-[40px] transition-all duration-200'>
       {/* Logo on the left */}
       <div className='logo bg-menu w-[8rem] mx-4 cursor-pointer ' onClick={() => router.push('/')}>
         <img src='/assets/linkUp.png' alt='Logo' className='cursor-pointer hover:brightness-95' />
+      </div>
+
+      {/* Mobile Hamburger Icon */}
+      <div className='sm:hidden ml-auto mx-4'>
+        <MenuOutlined
+          onClick={handleDrawerToggle}
+          style={{ fontSize: '24px', color: '#000' }} // Adjust the size and color
+        />
       </div>
 
       {/*  Menu */}
@@ -74,11 +88,28 @@ const Header = () => {
           selectedKeys={[current]}
           mode='horizontal'
           items={items}
-          className='menu-center w-full items-center justify-center bg-menu h-[70px]'
+          className='menu-center w-full items-center justify-center bg-menu h-[70px] hidden md:flex'
         />
       </ConfigProvider>
 
-      <div className='sign-in bg-menu w-[8rem] mx-4 sm:hidden'>{/* login button ?? */}</div>
+      <div className='sign-in bg-menu w-[8rem] mx-4 hidden md:flex'>
+        {/* Drawer for mobile menu */}
+      </div>
+
+      <Drawer
+        title='Menu'
+        placement='left'
+        closable={true}
+        onClose={handleDrawerToggle}
+        open={drawerVisible}>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode='inline'
+          items={items}
+          className='w-full'
+        />
+      </Drawer>
     </div>
   );
 };
